0e495b655d15b401b963f12c74bccfa770541bc8
Apprentice <apprentice@devbootcamp.com>
2012-11-12 14:21:29 -0800

    add styling to app

--- pollster/app/controllers/questions_controller.rb
+++ pollster/app/controllers/questions_controller.rb
@@ -1,2 +1,21 @@
 class QuestionsController < ApplicationController
+
+  before_filter :find_poll, only: [:create]
+
+  def create
+    @question = @poll.questions.new(params[:question])
+    if @question.save
+      redirect_to @poll
+    else
+      flash[:notice] = "Question was not created. Try again"
+      redirect_to 'new'
+    end
+  end
+
+private
+
+  def find_poll
+    @poll = Poll.find(params[:poll_id])
+  end
+
 end
