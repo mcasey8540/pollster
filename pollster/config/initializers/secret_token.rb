a1e61c611f10b23cd34204c5f9b68f6b4446f6ea
Apprentice <apprentice@devbootcamp.com>
2012-11-12 11:00:14 -0800

    create new rails app pollster

---
+++ pollster/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pollster::Application.config.secret_token = '98c04d27ca0a2a747a7709d8646e81da10006665c7275a2fd22d36ceaa321334a6184b62df588aaebc989917545d109999a0e8895aa4fbc456c5360af20c2dee'
